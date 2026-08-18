21aa38a234fe75744f06f97261babba8a14e9129
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-08-18 15:23:43 -0300

    Agregamos los modelos

---
+++ models/Propietario.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Inmobiliaria.Models
+{
+    public class Propietario
+    {
+        [Key]
+        public int IdPropietario { get; set; }
+        
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = "";
+        
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string Apellido { get; set; } = "";
+        
+        [Required(ErrorMessage = "El DNI es obligatorio")]
+        public string Dni { get; set; } = "";
+        
+        public string? Telefono { get; set; }
+        
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; } = "";
+        
+        public string? Clave { get; set; }
+    }
+}
