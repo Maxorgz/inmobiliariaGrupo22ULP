5a486957f50b1032e47d83320d906003426a0437
Danirodriguez45B <danielrodriguez45b@gmail.com>
2026-08-20 17:51:24 -0300

    sql connector

--- InmobiliariaWeb/obj/Debug/net10.0/InmobiliariaWeb.AssemblyInfo.cs
+++ InmobiliariaWeb/obj/Debug/net10.0/InmobiliariaWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231b0d88d56f85878abfe2f550ea0256fe125812")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b66c6a32f83ad580ebda12e88415bd7521a9fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -10,11 +10,3 @@
 A continuación se presenta el esquema del modelo de datos correspondiente a la aplicación:
 ### Diagrama Entidad-Relación (DER) / Diagrama de Clases
 ![Diagrama del Proyecto](./diagrama/inmobiliariagrupo22.png)
-> **Nota:** Puedes adjuntar la imagen en el repositorio (por ejemplo, en una carpeta `/docs` o `/img`) y enlazarla como se muestra arriba, o pegar directamente un diagrama generado en Mermaid.
-<details>
-<summary>Ver diagrama en código Mermaid (Opcional)</summary>
-```mermaid
-erDiagram
-    USUARIO ||--o{ PEDIDO : realiza
-    PEDIDO ||--|{ DETALLE_PEDIDO : contiene
-    PRODUCTO ||--o{ DETALLE_PEDIDO : pertenece
