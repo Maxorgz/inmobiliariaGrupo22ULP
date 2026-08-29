69f40ac1f058fe7ad8d187baf22ac78fd2d711f5
Juani-ds <juanignaciogenero@gmail.com>
2026-08-29 00:42:57 -0300

    Agregados regex de inquilino

--- InmobiliariaWeb/Models/Inquilino.cs
+++ InmobiliariaWeb/Models/Inquilino.cs
@@ -8,6 +8,7 @@ namespace InmobiliariaWeb.Models
         public int IdInquilino { get; set; }
         
         [Required(ErrorMessage = "El DNI es obligatorio")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "El DNI debe tener 8 digitos.")]
         public string Dni { get; set; } = "";
         
         [Required(ErrorMessage = "El nombre es obligatorio.")]
@@ -15,8 +16,10 @@ namespace InmobiliariaWeb.Models
         public string Nombre { get; set; } = "";
         
         [Required(ErrorMessage = "El apellido es obligatorio")]
+        [RegularExpression(@"^[A-Za-z]{2,15}$", ErrorMessage = "El apellido debe tener entre 2 y 15 caracteres.")]
         public string Apellido { get; set; } = "";
         
+        [RegularExpression(@"^(?=.*[0-9]).{10}$", ErrorMessage = "El telefono debe tener 10 digitos.")]
         public string? Telefono { get; set; }
         
         [Required(ErrorMessage = "El email es obligatorio")]
